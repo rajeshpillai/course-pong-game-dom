6f0d524f65fed09c5d52010ae17a27aacb1c13a9
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-14 15:34:02 +0530

    paddle.js

--- game/paddle.js
+++ game/paddle.js
@@ -1,20 +1,21 @@
-function Paddle() {
+function Paddle(game) {
     Entity.call(this);
+    this.game = game;
 
     this.width = 20;
     this.height = 100;
     this.score = 0;
     this.speed = 10;
 
-    //this.y = game.height / 2 - this.height; // center vertically
+    this.y = game.height / 2 - this.height; // center vertically
 }
 
 Paddle.prototype = Object.create(Entity.prototype);
 Paddle.prototype.constructor = Paddle;
 
-Paddle.prototype.update = function (game) {
+Paddle.prototype.update = function () {
     Entity.prototype.update.apply(this, arguments);
 
     // y will always stay below or equal to 0
-    this.y = Math.min(Math.max(this.y, 0), game.height - this.height);
+    this.y = Math.min(Math.max(this.y, 0), this.game.height - this.height);
 }
