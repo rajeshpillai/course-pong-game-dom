49df20b96df7a8ca6abc84a821069a55d1514e02
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-14 14:58:08 +0530

    Creating paddle.js (base class/ctor for Player and Bot

--- game/paddle.js
+++ game/paddle.js
@@ -6,5 +6,15 @@ function Paddle() {
     this.score = 0;
     this.speed = 10;
 
-    this.y = game.height / 2 - this.height; // center vertically
+    //this.y = game.height / 2 - this.height; // center vertically
+}
+
+Paddle.prototype = Object.create(Entity.prototype);
+Paddle.prototype.constructor = Paddle;
+
+Paddle.prototype.update = function (game) {
+    Entity.prototype.update.apply(this, arguments);
+
+    // y will always stay below or equal to 0
+    this.y = Math.min(Math.max(this.y, 0), game.height - this.height);
 }
