7b372a8d57075b5fd20d5d2db4bb4a5bb42d7861
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-15 21:20:51 +0530

    Adding Score

--- game/start.js
+++ game/start.js
@@ -6,7 +6,8 @@ var canvas = document.getElementById("game"),
 game.entities = [
     game.player = new Player(game),
     game.bot = new Bot(game),
-    game.ball = new Ball(game)
+    game.ball = new Ball(game),
+    game.score = new Score(game)
 ];
 
 game.start(); // Start will trigger game loop
